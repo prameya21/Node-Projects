edab9e88667d1261c94856fdb6832cbdd18d9d8b
prameya21 <prameya.kirtikar@gmail.com>
2020-07-24 22:11:22 -0700

    egrep fail

--- Projects/egrep/egrep.js
+++ Projects/egrep/egrep.js
@@ -1,52 +1,59 @@
 
-var filePath="C:\\Users\\Daemon\\eclipse-workspace\\InterviewPrep\\src";
+var filePath="/home/prameya/Code/Leetcode-problem-Sets/src";
 
-var textToSearch="main";
+var textToSearch="dfs";
 
 var path=require('path');
 var fs=require('fs');
 
 var ret=[];
 
+function getFiles(path,regex)
+{
+  fs.readdir( filePath, function( err, files)
+  {
+    if( err )
+    {
+      console.log("Error reading files: ", err);
+    }
+    else
+    {
+      // keep track of how many we have to go.
+      var remaining = files.length;
+      var totalBytes = 0;
+      if(files.length==0)
+      {
+        console.log("Directory emnpty");
+        return ret;
+      }
+      console.log(remaining);
 
-fs.readdir( filePath, function( err, files) {
-        if ( err )
+      files.forEach(function(file){
+      fs.readFile(filePath+"/"+file, 'utf8',function( error, data )
+      {
+        if ( error )
         {
-            console.log("Error reading files: ", err);
+          console.log("Error: ", error);
         }
         else
         {
-            // keep track of how many we have to go.
-            var remaining = files.length;
-            var totalBytes = 0;
-            if(files.length==0)
-            {
-              console.log("Directory emnpty");
-              return ret;
-            }
-            console.log(remaining);
-
-            files.forEach(function(file){
-              fs.readFile(filePath+"\\"+file, 'utf8',function( error, data ) {
-                  if ( error )
-                  {
-                      console.log("Error: ", error);
-                  }
-                  else
-                  {
-                      if(data.includes(textToSearch))
-                      {
-                        //console.log(file);
-                        ret.push(file);
-                      }
-                  }
-                  remaining-=1;
-                  if(remaining==0)
-                  {
-                    console.log(ret.length);
-                  }
-              });
-            });
+          if(data.includes(textToSearch))
+          {
+            ret.push(file);
           }
-          return ret;
+        }
+        remaining-=1;
+        if(remaining==0)
+        {
+            //console.log(ret);
+            return ret;
+        }
+      });
     });
+  }
+
+  });
+}
+
+var op=getFiles(filePath,textToSearch);
+console.log(op);
